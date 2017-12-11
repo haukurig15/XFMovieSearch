afe676d10110aee3f123d5d5c0e7cde6196aac55
haukurig15 <haukurig15@ru.is>
2017-12-11 13:59:55 +0000

    creates new XF template and adds connection to database

---
+++ XFMovieSearch/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DM.MovieApi;
+using DM.MovieApi.MovieDb.Movies;
+using MovieDatabase;
+using Xamarin.Forms;
+
+namespace XFMovieSearch
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MovieDbFactory.RegisterSettings(new MovieDbSettings());
+            var movieApi = MovieDbFactory.Create<IApiMovieRequest>().Value;
+            var movieService = new MovieServices(movieApi);
+
+            MainPage = new XFMovieSearchPage(movieService);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XFMovieSearch/XFMovieSearchPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MovieDatabase;
+using Xamarin.Forms;
+
+namespace XFMovieSearch
+{
+    public partial class XFMovieSearchPage : ContentPage
+    {
+        private MovieServices _movieService;
+
+        public XFMovieSearchPage(MovieServices movieService)
+        {
+            this._movieService = movieService;
+            InitializeComponent();
+        }
+
+        private async void GetMoviesButton_OnClicked(object sender, EventArgs e)
+        {
+            var movieResult = await _movieService.getListOfMoviesMatchingSearch(MovieEntry.Text);
+            this.MovieLabel.Text = movieResult[0].Title; 
+        }
+    }
+}
