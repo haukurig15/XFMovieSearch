fd425ae7a7909284f4dc138ee4e517f60e7ae871
haukurig15 <haukurig15@ru.is>
2017-12-12 13:12:38 +0000

    adds a movie list page

---
+++ XFMovieSearch/MovieListPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MovieDatabase;
+using Xamarin.Forms;
+
+namespace XFMovieSearch
+{
+    public partial class MovieListPage : ContentPage
+    {
+        public MovieListPage(Movie movie)
+        {
+            this.BindingContext = movie;
+            InitializeComponent();
+        }
+    }
+}
